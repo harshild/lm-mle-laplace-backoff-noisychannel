b5e11020db08f560d823ad4fa40b07515eab27aa
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2019-10-10 22:44:03 -0400

    Updated code with Pojo for language model

--- language_models.py
+++ language_models.py
@@ -1,10 +1,48 @@
 import sys
 from collections import Counter
 import pickle
-
 import numpy
-
 from dataset_parser import parse_conllu_dataset, findtokens, make_asymetric_pairs
+
+
+class LanguageModel:
+    def __init__(self, model_name, N, hyper_parameters, model_data):
+        self.model_name = model_name
+        self.N = N
+        self.hyper_parameters = hyper_parameters
+        self.model_data = model_data
+
+    @property
+    def model_name(self):
+        return self._model_name
+
+    @property
+    def N(self):
+        return self._N
+
+    @property
+    def hyper_parameters(self):
+        return self._hyper_parameters
+
+    @property
+    def model_data(self):
+        return self._model_data
+
+    @hyper_parameters.setter
+    def hyper_parameters(self, value):
+        self._hyper_parameters = value
+
+    @model_name.setter
+    def model_name(self, value):
+        self._model_name = value
+
+    @N.setter
+    def N(self, value):
+        self._N = value
+
+    @model_data.setter
+    def model_data(self, value):
+        self._model_data = value
 
 
 def prepare_bigram_input(sentence_list):
@@ -80,31 +118,29 @@ def calculate_perplexity(N, model, conllu_data_tune):
     perplexity = 0
     if N == 1:
         types = Counter(tokens)
-        sum = 0
+        sum_p = 0
         for w in types:
             if model.keys().__contains__(w):
-                sum = sum + (types[w] * (numpy.log(model[w])))
-        perplexity = sum / len(tokens)
+                sum_p = sum_p + (types[w] * (numpy.log(model[w])))
+        perplexity = sum_p / len(tokens)
     if N == 2:
         pairs = make_asymetric_pairs(sentence_list)
         unique_pairs = Counter(pairs)
-        sum = 0
+        sum_p = 0
         for w in unique_pairs:
             if model.keys().__contains__(w[0] + "|" + w[1]):
-                sum = sum + (unique_pairs[w] * (numpy.log(model[w[0] + "|" + w[1]])))
-        perplexity = sum / len(pairs)
+                sum_p = sum_p + (unique_pairs[w] * (numpy.log(model[w[0] + "|" + w[1]])))
+        perplexity = numpy.exp(-(sum_p / len(pairs)))
 
     return perplexity
 
 
 def main():
     mode = sys.argv[1]
-    model_name = None
-    hyperparameters = None
-    model = None
     conllu_data_tune = None
-    N = None
+    language_model = None
     if mode == "train":
+        hyper_parameters = None
         model_name = sys.argv[2]
         N = int(sys.argv[3])
         conllu_data_train = sys.argv[4]
@@ -112,35 +148,36 @@ def main():
         save_file = sys.argv[6]
 
         if sys.argv[7]:
-            hyperparameters = process_hyperparamters(sys.argv[7])
-
+            hyper_parameters = process_hyperparamters(sys.argv[7])
 
         sentence_list_train = parse_conllu_dataset(conllu_data_train)
 
-        model = {}
+        model_data = {}
         if model_name == "mle":
             if N == 1:
-                model = unigram_mle(sentence_list_train)
+                model_data = unigram_mle(sentence_list_train)
             if N == 2:
-                model = bigram_mle(sentence_list_train)
+                model_data = bigram_mle(sentence_list_train)
 
         if model_name == 'laplace':
             k = 1
-            if hyperparameters.keys().__contains__("k"):
-                k = int(hyperparameters["k"])
+            if hyper_parameters.keys().__contains__("k"):
+                k = int(hyper_parameters["k"])
 
             if N == 1:
-                model = unigram_laplace(sentence_list_train, k)
+                model_data = unigram_laplace(sentence_list_train, k)
             if N == 2:
-                model = bigram_laplace(sentence_list_train, k)
+                model_data = bigram_laplace(sentence_list_train, k)
 
-        pickle.dump(model, open(save_file, "wb"))
+        language_model = LanguageModel(model_name, N, hyper_parameters, model_data)
+        pickle.dump(language_model, open(save_file, "wb"))
 
     if mode == "eval":
-        model = pickle.load(open(sys.argv[2], "rb"))
-        conllu_data_tune=sys.argv[3]
-        N = 2
-    print(model_name, N, hyperparameters, calculate_perplexity(N, model, conllu_data_tune))
+        language_model = pickle.load(open(sys.argv[2], "rb"))
+        conllu_data_tune = sys.argv[3]
+    print(language_model.model_name, language_model.N, language_model.hyper_parameters,
+          calculate_perplexity(language_model.N, language_model.model_data, conllu_data_tune)
+          )
 
 
 if __name__ == '__main__':
